b6e27fd4739beaff52199b43da0d6ffdbf7df66a
TimotiusLeonardo <lionato14@gmail.com>
2021-08-02 15:08:01 +0700

    Initial Commit

---
+++ URLShortner/URLShortnerApp.swift
@@ -0,0 +1,17 @@
+//
+//  URLShortnerApp.swift
+//  URLShortner
+//
+//  Created by Timotius Leonardo Lianoto on 02/08/21.
+//
+
+import SwiftUI
+
+@main
+struct URLShortnerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
